1af148e3bf119a3942e9b932c80a3a8e05154dcd
Stephane Baroux <stephane.baroux@gmail.com>
2021-03-31 17:02:08 +0200

    Working Code
    JS script that creates a button, associates appropriate id tag to corresponding section id (for scroll-snap and button navigation to correct section).
    Sections must have class "section-structure" to run JS code and create button.
    Also added "push" class which sets relavent button to the right of navbar

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 var navButtons = document.getElementsByClassName("nav-btn");
 var menu1 = document.getElementById("menu1");
-var heroPage = document.getElementById("hero");
-var sectionStructure = document.getElementsByClassName("section-structure");
+var sectionElements = document.getElementsByClassName("section-structure");
+var navbar = document.getElementById("navbar-menu");
 
-
-function pressButton(){
-    heroPage.scrollIntoView();
+function buildSectionStructure(item){
+    // let itemId = navbar(item);
+    createButton(item);
 }
 
+function createButton(i){
+    let btn = document.createElement("li");
+    let sectionId = sectionElements[i].id;
+    let sectionTitle = sectionElements[i].title;
+    // let sectionDataLayout = sectionElements[i].getAttribute(data-layout);
+    btn.appendChild(document.createTextNode(sectionTitle));
+    btn.classList.add("nav-btn");
+    btn.classList.add(sectionId + "-class");
+    if (sectionElements[i].classList.contains("push")){
+
+        btn.classList.add("push");
+    }
+
+    navbar.appendChild(btn);
+
+    btn.addEventListener("click", function(){
+        buttonClicked(i);
+    }, false);
+
+}
+
+function buttonClicked(i){
+    sectionElements[i].scrollIntoView();
+ }
 
 
-menu1.addEventListener("click", pressButton);
+for (var element=0 ; element < sectionElements.length; element++ ) { 
+    buildSectionStructure(element);
+}
